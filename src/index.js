2b10179f2e4865da059306cd8c281d753d4360db
Julia Pritychenko <juliasnoww@mail.ru>
2018-09-22 20:13:04 +0300

    task 2

--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  obj=preferences;
+    obj.unshift(0);
+    keys = Object.keys(obj);
+    var result = 0;
+        for(var i = 0;i < preferences.length;i++)
+    {
+        first =keys[i];
+        second=obj[first];
+        third=obj[second];
+       // console.log(first+"->"+second+"->"+third+"->"+obj[third]);
+        if (first==obj[third] && (first != obj[first] && second != obj[second] && third != obj[third])){result++}
+    }
+    return result/3;
 };
